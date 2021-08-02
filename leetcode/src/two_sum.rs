412b85e8e93372d4415751018979df23248618ba
William Evieux <wevieux@gmail.com>
2021-08-02 18:09:12 +0200

    Leetcode #1 : Two sum much better RAM & computing

--- leetcode/src/two_sum.rs
+++ leetcode/src/two_sum.rs
@@ -1,24 +1,20 @@
 // Benchmark : 
 // Runtime faster than 100.00%
-// Memory usage : 2.3 MB less than 35.88%
+// Memory usage : 2.3 MB less than 79.07%
 
 struct Solution;
 
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
         let mut cache = std::collections::HashMap::with_capacity(nums.len());
-        let mut answer = vec![0, 0];
-        // We store each values in hashmap
-        // For each number we then check if, when substracted by target, we find a corresponding number
-        // in our hashmap, if yes, then this found number and the one we substracted are the two answer index
         for (i, _n) in nums.iter().enumerate() {
             if cache.get(&(target - nums[i])) != None {
-                answer = vec![*cache.get(&(target - nums[i])).unwrap(), i as i32];
+                return vec![*cache.get(&(target - nums[i])).unwrap(), i as i32];
             } else {
                 cache.insert(nums[i], i as i32);
             }
         }
-        answer
+        vec![]
     }
 }
 
